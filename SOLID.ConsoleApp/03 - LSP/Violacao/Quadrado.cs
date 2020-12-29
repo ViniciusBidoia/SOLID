78f5f93d3e7a0db7faea5384662f4357bea68970
Vinicius Bidoia <vinicius.bidoia@hotmail.com>
2020-12-28 23:46:55 -0300

    Violacao LSP

---
+++ SOLID.ConsoleApp/03 - LSP/Violacao/Quadrado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID.ConsoleApp.LSP.Violacao
+{
+    public class Quadrado : Retangulo
+    {
+        public override double Altura
+        {
+            set { base.Altura = base.Largura = value; }
+        }
+
+        public override double Largura
+        {
+            set { base.Altura = base.Largura = value; }
+        }
+    }
+}
