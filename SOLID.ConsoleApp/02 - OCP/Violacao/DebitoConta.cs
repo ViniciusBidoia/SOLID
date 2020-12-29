042839b32ef70ee7987212e536ba6349871a0081
Vinicius Bidoia <vinicius.bidoia@hotmail.com>
2020-12-28 23:06:29 -0300

    OCP Violacao

---
+++ SOLID.ConsoleApp/02 - OCP/Violacao/DebitoConta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID.ConsoleApp.OCP.Violacao
+{
+    /// <summary>
+    /// Exemplo de violacao, caso tivesse mais tipos de contas no fututo, seria necessario modificar a classe, quebrando o conceito do ABERTO-FECHADO
+    /// </summary>
+    public class DebitoConta
+    {
+        public void Debitar(decimal valor, string conta, TipoConta tipoConta)
+        {
+            if (tipoConta == TipoConta.Corrente)
+            {
+                // Debita Conta Corrente
+            }
+
+            if (tipoConta == TipoConta.Poupanca)
+            {
+                // Valida Aniversário da Conta
+                // Debita Conta Poupança
+            }
+        }
+    }
+}
