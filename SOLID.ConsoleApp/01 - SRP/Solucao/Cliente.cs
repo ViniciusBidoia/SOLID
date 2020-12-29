07f1cbdb73fefc81adb1be6f7e9ca6150c35982f
Vinicius Bidoia <vinicius.bidoia@hotmail.com>
2020-12-28 23:02:13 -0300

    Finalizando exemplo do SRP, não é a versao final pois falta implementar outras metodologiuas que serao vistar nos outros princiopios do SOLID

---
+++ SOLID.ConsoleApp/01 - SRP/Solucao/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID.ConsoleApp.SRP.Solucao
+{
+    public class Cliente
+    {
+        public Guid ClienteId { get; set; }
+        public string Nome { get; set; }
+        public Email Email { get; set; }
+        public CPF Cpf { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        /// <summary>
+        /// Responsabilidade de validar email e cpf passam a estar na suas respoectivas classes
+        /// </summary>
+        /// <returns></returns>
+        public bool Validar()
+        {
+            return Email.Validar() && Cpf.Validar();
+        }
+    }
+}
